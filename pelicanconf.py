0ae4cb6543c7d71639b8ec908484f3db67899e54
jul10l1r4 <jul10l1r4@disroot.org>
2020-01-14 13:43:29 -0300

    Pelicant local to gh

---
+++ pelicanconf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Julio Lira'
+SITENAME = 'Comuna brasileira'
+SITEURL = 'bolivar-comuna.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = 'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Jornal A Verdade', 'http://averdade.org.br/'),
+         ('Unidade Popular', 'https://unidadepopular.org.br/'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'https://twitter.com/jul10l1r4'),
+          ('Telegram', 't.me/juraul'),)
+
+DEFAULT_PAGINATION = 8
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
